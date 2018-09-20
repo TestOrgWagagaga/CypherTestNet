96940134e216840b38b5579252e41f13b3afd031
Ares Xu <xy008areshsu@gmail.com>
2018-09-19 23:57:08 -0700

    Update start.sh

--- build/start.sh
+++ build/start.sh
@@ -1,1 +1,1 @@
-~/cypher --onetdebug 1 --onetdir ~/ipaddr/private.toml --publickeydir ~/group.toml --datadir ~/cypher_db --networkid 123666 --port 7000 --rpcport 8000 --verbosity 4 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcapi eth,web3,personal --nodiscover 2>$co/output.log &
+/home/ubuntu/cypher --onetdebug 1 --onetdir /home/ubuntu/ipaddr/private.toml --publickeydir /home/ubuntu/group.toml --datadir /home/ubuntu/cypher_db --networkid 123666 --port 7000 --rpcport 8000 --verbosity 4 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcapi eth,web3,personal --nodiscover 2>$co/output.log &
