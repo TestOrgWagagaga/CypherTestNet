3d615e42de300f1c8c48eb50d4b8b4ae424343af
Ares Xu <xy008areshsu@gmail.com>
2018-09-20 01:11:14 -0700

    Update preinstall.sh

--- build/preinstall.sh
+++ build/preinstall.sh
@@ -1,5 +1,6 @@
-sudo apt install make
-sudo apt install golang-go
+sudo apt -y install make
+sudo apt -y install golang-1.10-go
+sudo ln -s /usr/lib/go-1.10/bin/go /usr/bin/go
 
 rm -rf /home/ubuntu/go-cypherium
 mkdir /home/ubuntu/go-cypherium
