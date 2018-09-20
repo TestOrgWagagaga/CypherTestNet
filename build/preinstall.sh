b9c05548c1eb9c0d7dfcc8f93c2d96652c7bee72
Ares Xu <xy008areshsu@gmail.com>
2018-09-20 04:01:22 -0700

    Update preinstall.sh

--- build/preinstall.sh
+++ build/preinstall.sh
@@ -8,5 +8,6 @@ sudo ln -s /usr/lib/go-1.10/bin/go /usr/bin/go
 sudo rm -rf /home/ubuntu/go-cypherium
 sudo rm -rf /home/ubuntu/genesis.json
 sudo rm -rf /home/ubuntu/group.toml
+sudo rm -rf /home/ubuntu/cypher_db
 
 mkdir /home/ubuntu/go-cypherium
