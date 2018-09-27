580bdfe722aa05a91b65d179fe2f5cf6086aab67
Luke <35581017@qq.com>
2018-09-27 15:23:35 +0800

    test10

--- build/start.sh
+++ build/start.sh
@@ -1,2 +1,2 @@
 cd /home/ubuntu
-./cypher --onetdebug 1 --onetdir ./ipaddr/private.toml --publickeydir group.toml --datadir cypher_db --networkid 123666 --port 7000 --rpcport 8000 --verbosity 4 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcapi eth,web3,personal > /dev/null 2>&1 &
+./cypher --onetdebug 1 --onetdir ./ipaddr/private.toml --publickeydir group.toml --datadir cypher_db --networkid 123666 --port 7000 --rpcport 8000 --verbosity 4 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcapi cph,web3,personal > /dev/null 2>&1 &
